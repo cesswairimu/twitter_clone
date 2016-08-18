c985f3366b65e452843255571c5ab41663c3b2ea
Cess <cessmbuguar@gmail.com>
2016-08-18 12:01:22 +0300

    validating jina, pepe

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :jina, presence: true, length: { maximum: 20 }
+  validates :pepe, presence: true, length: { maximum: 30 },
+            uniqueness: true
+  
 end
