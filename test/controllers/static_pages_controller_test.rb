7324bb85f346f4f33179762b1941357f60cffae1
Cess <cessmbuguar@gmail.com>
2016-08-25 22:39:41 +0300

    testing the titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
+    assert_select "title", "Home | TwitterClone"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
+    assert_select "title", "About | TwitterClone"
   end
 
   test "should get help" do
     get help_url
     assert_response :success
+    assert_select "title", "Help | TwitterClone"
   end
 
 end
