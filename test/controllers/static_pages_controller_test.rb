10f418f62b4ec01069539fb53ebe7ebbf940d33a
Cess <cessmbuguar@gmail.com>
2016-08-25 23:11:19 +0300

    define setup for the default title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @title = "TwitterClone"
+  end
+
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | TwitterClone"
+    assert_select "title", "Home | #{@title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | TwitterClone"
+    assert_select "title", "About | #{@title}"
   end
 
   test "should get help" do
     get help_url
     assert_response :success
-    assert_select "title", "Help | TwitterClone"
+    assert_select "title", "Help | #{@title}"
   end
 
 end
