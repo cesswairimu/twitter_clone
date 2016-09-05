5b2d650c7c06908d78e2cefda7cbf4fc6feca62e
Cess <cessmbuguar@gmail.com>
2016-09-05 22:59:34 +0300

    add show action and corrected the input params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       flash[:mmmh]="You made it"
+      redirect_to @user
     else 
       render 'new'
     end
+  end
+  def show
+    @user = User.find(params[:id])
   end
 
   private
