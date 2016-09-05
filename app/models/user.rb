cd8af69b171240058b44ad0e89f0fde4e729f112
Cess <cessmbuguar@gmail.com>
2016-09-05 23:10:56 +0300

    validating password and hashing a password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   validates :jina, presence: true, length: { maximum: 20 }
   validates :pepe, presence: true, length: { maximum: 30 },
             uniqueness: true
+  has_secure_password
+  validates :password, presence:true, length:{ minimum: 8 }
   
 end
