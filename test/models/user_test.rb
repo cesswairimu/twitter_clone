ef6ce7d87953db3e7bc05ed487abe471cc3926fa
Cess <cessmbuguar@gmail.com>
2016-09-06 21:17:30 +0300

    testing validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(jina: "Robin", pepe: "robin@gmail.com", password: "lawrence", 
+                     password_confirmation: "lawrence")
+  end
+  test "jina should be present" do
+    @user.jina = " "
+    assert_not @user.valid?
+  end
+  test "pepe should be present" do
+    @user.pepe = " "
+    assert_not @user.valid?
+  end
+  test "password should have minimum length" do
+    @user.password = "c" * 5
+    assert_not @user.valid?
+  end
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be identical to password confirmation"do
+  #@user.password !== @user.password_confirmation
+  assert_not @user.valid?
+end
 end
