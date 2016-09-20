6a51d959ad346abdb51436732820c0a2ef502bb0
Cess <cessmbuguar@gmail.com>
2016-09-20 19:52:03 +0300

    change root_path to root_url

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,7 +4,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "navigation links should be okay" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]",root_path 
+    assert_select "a[href=?]",root_url
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", signup_path
