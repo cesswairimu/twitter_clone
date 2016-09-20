2eaab61ad2673bfc85a09f1380b224823940ca40
Cess <cessmbuguar@gmail.com>
2016-09-20 19:52:53 +0300

    test for unsuccessful signups

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -2,7 +2,19 @@ require 'test_helper'
 
 class UserSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
-  test "should redirect new unsuccessful signup" do
+  test "should redirect new upon unsuccessful signup" do
+    get signup_path
+    assert_template 'users/new'
+    assert_no_difference 'User.count' do 
+
+    
+    post users_path,  params: {user: { jina: "wewe", pepe: "me@yuu", password: "eXampple", password_confirmation: "example"
+    }}
+    end
+    
+    assert_not flash.empty?
+    assert_template 'users/new'
+    end
 
     
    
