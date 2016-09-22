70401b80699e3b9a622c6dbc3bf42102e60e12a8
Cess <cessmbuguar@gmail.com>
2016-09-22 19:11:03 +0300

    test for successful signup

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UserSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
   test "should redirect new upon unsuccessful signup" do
     get signup_path
     assert_template 'users/new'
@@ -15,6 +14,19 @@ class UserSignupTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_template 'users/new'
     end
+  test "should save user on successful signup" do
+    get signup_path
+    assert_template 'users/new'
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          jina:"Nitram", pepe: "nitram@gmail.com", password: "cessitoo", password_confirmation: "cessitoo"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 
     
    
